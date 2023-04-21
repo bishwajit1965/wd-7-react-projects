48d041c599fa02d1418128ae0fac6b9a0e0b2909
bishwajit1965 <paul.bishwajit09@gmail.com>
2023-04-21 22:18:33 +0600

    Almost done

--- REACT_SERVER_SIDE_PROJECTS/the-news-dragon-server/index.js
+++ REACT_SERVER_SIDE_PROJECTS/the-news-dragon-server/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const categories = require("./data/categories.json");
+const news = require("./data/news.json");
+
 const port = process.env.PORT || 5000;
 
 app.use(cors());
@@ -11,6 +13,31 @@ app.get("/", (req, res) => {
 });
 app.get("/categories", (req, res) => {
   res.send(categories);
+});
+app.get("/news", (req, res) => {
+  res.send(news);
+});
+
+// Id wise specific news
+app.get("/news/:id", (req, res) => {
+  const id = req.params.id;
+  const particularNews = news.find((pNews) => pNews._id === id);
+  res.send(particularNews);
+});
+
+// category wise news
+app.get("/categories/:id", (req, res) => {
+  const id = parseInt(req.params.id);
+  if (id === 0) {
+    res.send(news);
+  } else if (id === 8) {
+    res.send(news);
+  } else {
+    const categoryWiseNews = news.filter(
+      (cWiseNews) => parseInt(cWiseNews.category_id) === id
+    );
+    res.send(categoryWiseNews);
+  }
 });
 
 app.listen(port, () => {
